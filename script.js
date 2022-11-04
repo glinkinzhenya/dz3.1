20addfbac438886b329032e113caa8196120c7e5
glinkinzhenya <glinkinzhenya@gmail.com>
2022-11-04 16:58:25 +0200

    2

--- script.js
+++ script.js
@@ -2,4 +2,4 @@ numberFirst = +prompt("введи число").trim();
 numberSecond = +prompt("введи число").trim();
 numberThird = +prompt("введи число").trim();
 sum = (numberFirst + numberSecond + numberThird) / 3;
-alert(`Среднее арефмитическое чисел ${numberFirst}, ${numberSecond}, ${numberThird}, будет ${sum}.`);
+alert(`Среднее арифмитическое чисел ${numberFirst}, ${numberSecond}, ${numberThird}, будет ${sum}.`);
