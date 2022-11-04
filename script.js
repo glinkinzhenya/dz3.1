4020df2d28f11c577afa22ed8c870d8d04001c23
glinkinzhenya <glinkinzhenya@gmail.com>
2022-11-04 16:53:28 +0200

    1

---
+++ script.js
@@ -0,0 +1,5 @@
+numberFirst = +prompt("введи число").trim();
+numberSecond = +prompt("введи число").trim();
+numberThird = +prompt("введи число").trim();
+sum = (numberFirst + numberSecond + numberThird) / 3;
+alert(`Среднее арефмитическое чисел ${numberFirst}, ${numberSecond}, ${numberThird}, будет ${sum}.`);
